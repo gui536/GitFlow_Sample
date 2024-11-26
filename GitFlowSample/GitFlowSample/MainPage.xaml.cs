0031be7536a952512a61a65b87ad0f49e3d52224
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-11-26 17:59:42 -0300

    feat: Created Obrigado function on OnCounterClicked

--- GitFlowSample/GitFlowSample/MainPage.xaml.cs
+++ GitFlowSample/GitFlowSample/MainPage.xaml.cs
@@ -2,8 +2,6 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
@@ -11,13 +9,7 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
+            CounterBtn.Text = $"Obrigado por clicar!";
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
