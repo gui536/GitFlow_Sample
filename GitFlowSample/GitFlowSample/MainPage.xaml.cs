125dbe53706af68cc09cc3a6edcae82047ca7f21
Guilherme <guilherme.santos536@etec.sp.gov.br>
2024-11-26 20:45:30 -0300

    feat: Ajudei

--- GitFlowSample/GitFlowSample/MainPage.xaml.cs
+++ GitFlowSample/GitFlowSample/MainPage.xaml.cs
@@ -1,17 +1,1 @@
-﻿namespace GitFlowSample
-{
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            CounterBtn.Text = $"Obrigado por clicar!";
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
-    }
-
-}
+﻿
